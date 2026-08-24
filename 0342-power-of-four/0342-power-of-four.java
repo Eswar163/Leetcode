50691ad168e4faeaeeee5bdc7cd7933bdd588f33
Eswar163 <eswargorjila3@gmail.com>
2026-08-24 22:30:59 +0530

    Time: 1 ms (89.75%), Space: 42.7 MB (33.69%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        return power(n);
+    }
+    boolean power(int n){
+        if(n==1){
+            return true;
+        }else if(n<=0||n%4!=0){
+            return false;
+        }
+        return power(n/4);
+    }
+}
