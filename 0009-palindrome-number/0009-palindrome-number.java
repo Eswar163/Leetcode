83cbe716d33aab097afa3b12b21c4ce3346748df
Eswar163 <eswargorjila3@gmail.com>
2026-08-25 22:16:28 +0530

    Time: 7 ms (10.47%), Space: 46.1 MB (33.18%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,21 @@
+class Solution {
+     int rev(int n){
+        int digits=(int)(Math.log10(n)+1);
+        return helper(n,digits);
+    }
+    int helper(int n,int digits){
+        if(n<10){
+            return n;
+        }
+        int rem=n%10;
+        return rem*(int)Math.pow(10, digits - 1)
+                + helper(n / 10, digits - 1);
+    }
+   
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        return x==rev(x);
+    }
+}
