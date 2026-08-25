c3cbc9e3fd878260d65b63580be386fcf6837923
Eswar163 <eswargorjila3@gmail.com>
2026-08-25 22:26:37 +0530

    Time: 5 ms (81.98%), Space: 46 MB (33.18%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,17 +1,13 @@
 class Solution {
-     int rev(int n){
-        int digits=(int)(Math.log10(n)+1);
-        return helper(n,digits);
-    }
-    int helper(int n,int digits){
-        if(n<10){
-            return n;
+    int sum=0;
+    int rev(int n){
+        if(n==0){
+            return sum;
         }
         int rem=n%10;
-        return rem*(int)Math.pow(10, digits - 1)
-                + helper(n / 10, digits - 1);
+        sum=sum*10+rem;
+        return rev(n/10);
     }
-   
     public boolean isPalindrome(int x) {
         if(x<0){
             return false;
